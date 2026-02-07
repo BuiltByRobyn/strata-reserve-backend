56df7ac05657e094260d8c7b264832dcd47f93b0
Ben Chan <bchanhp@gmail.com>
2026-02-07 00:04:14 -0800

    Fix 'Loading...' issue

--- supabase_migrations/008_add_company_name_to_profiles.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add company_name to profiles table
--- This allows storing an associated company directly on the user profile
-
-ALTER TABLE profiles
-ADD COLUMN IF NOT EXISTS company_name VARCHAR(255);
