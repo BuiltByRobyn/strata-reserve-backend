6b91ccdb7e1a097bee045da64fdb8dead15bdf80
Robyn <downie.robyn@hotmail.co.uk>
2026-02-12 21:24:41 -0800

    type error

--- src/client/routes/clientRoutes.ts
+++ src/client/routes/clientRoutes.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
-import { getClientDashboard, getClientReports } from '../controllers/clientController';
+import { getClientDashboard, getClientReports, getActiveServiceRequest } from '../controllers/clientController';
 
 const clientRoutes = new Hono();
 
 clientRoutes.get('/dashboard', getClientDashboard);
 clientRoutes.get('/reports', getClientReports);
+clientRoutes.get('/service-requests/active', getActiveServiceRequest);
 
 export { clientRoutes };
