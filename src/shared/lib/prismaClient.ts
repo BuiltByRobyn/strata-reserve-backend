f82d165e7c078f442a0d8452f056d99cfd7c1af5
Robyn <downie.robyn@hotmail.co.uk>
2026-03-25 13:15:41 -0700

    pool bug fix

--- src/shared/lib/prismaClient.ts
+++ src/shared/lib/prismaClient.ts
@@ -9,7 +9,7 @@ if (!connectionString) {
   throw new Error('DATABASE_URL environment variable is not set');
 }
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, max: 3 });
 
 const adapter = new PrismaPg(pool);
 
